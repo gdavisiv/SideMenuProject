12fa74fcfd875490344c2eb9695354b7f97d7b6e
George Davis IV <GDavisIV@gmail.com>
2020-04-07 17:06:22 -0400

    Creating the class needed to list menu options

--- SideMenuProject/ViewController.swift
+++ SideMenuProject/ViewController.swift
@@ -19,8 +19,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Assign menu to sidemenunavigationcontroller, and it get initialized with the root view controller
+        //For now the rootviewcontroller is a simple UIViewController that we initialize
+        menu = SideMenuNavigationController(rootViewController: UIViewController())
+        
+        //THis makes the menu on the left side
+        menu?.leftSide = true
+        
+        //Tell the manager what side the menu is on
+        //To make this menu 'swipe to open'
+        SideMenuManager.default.leftMenuNavigationController = menu
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+        
+        
+        }
+        
+        //setup did tap option'
+        @IBAction func didTapMenu() {
+            present(menu!, animated: true)
+    
     }
-
-
 }
-
+//Create a class that will hold all the items in our menu
+//class with a Subclass
+class MenuListController: UITableViewController {
+    //Create an array of items, itemms in our menu
+    var items = ["First", "Second", "Third", "ASDF", "DTGEW", "sadfewr"]
+    
+    //Override View did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //register a simple cell as this
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    //Implement the number of rows, which will be equivilent to the number of items in the array
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+        
+        //
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = items[indexPath.row]
+            return cell
+        }
+        
+    }
+    
+}
