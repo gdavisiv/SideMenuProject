08cda104dcf60bb3c176baf0d600d4516757bcc9
George Davis IV <GDavisIV@gmail.com>
2020-04-07 11:37:41 -0400

    Initial Commit

---
+++ SideMenuProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenuProject
+//
+//  Created by George Davis IV on 4/7/20.
+//  Copyright © 2020 3DMations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
